0df7690199ac2bbe2e54fc99e9c0c3fe5938b3ed
Gjorge <dimitrov.gjorge@protonmail.com>
2023-03-08 19:17:09 +0100

    code added part 2

--- G11/Class_05/server.js
+++ G11/Class_05/server.js
@@ -3,6 +3,22 @@ import express from "express";
 
 const app = express();
 
+//Middleware
+// Simple middleware
+app.use((req, res, next) => {
+
+    console.log("We are at middleware, We intercept each request made to the server =)");
+    //without next() the request is stuck;
+
+    // after the middleware finish it's job
+    // we invoke the method next() that will let the app reach the next/following logic
+    next(); 
+})
+
+
+//Exercise #2: Create middleware to show time of the request
+
+
 //Creating routes
 //This route will be hit with GET method
 app.get("/", (request, response) => { // localhost:3000 => default route
@@ -20,6 +36,37 @@ app.get("/about", (request, response) => {
 
 //Exerice #1: Create get route /student_info;
 //in h1 elemet return tot he user your fullname; age and subject that you study
+app.get("/student_info", (request, response) => {
+    response.send("<h1>Fullname: Gjorge Dimitrov; 28; NodeJs</h1>")
+})
+
+//We can return JSON; request; response
+app.get("/students", (req, res) => {
+    const students = [
+        {id: 1, name: "Lebron", age: 28, gender: "male"},
+        {id: 2, name: "Lesa Lesly", age: 27, gender: "female"}
+    ];
+
+    // res.setHeader("Content-Type", 'application/json');
+    res.send(JSON.stringify(students))
+})
+
+// * is WILDCARD MEANS ALL REST
+// Every non-existing route
+// will hit here =)
+app.get("*", (req, res) => {
+    // res.status(404)
+    // res.send("<h1>HTTP: 404 NOT FOUND AMIGO</h1>")
+
+    //same as above in one line
+    // res.status(404).send("<h1>HTTP: 404 NOT FOUND AMIGO</h1>")
+
+    // res.status(404).send({err_message: "PAGE NOT FOUND"});
+
+    res.redirect("/");
+ 
+})
+
 
 app.listen(3000, () => {
     console.log("Server is up and running...");
