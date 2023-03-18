8f94a22c07655bb7be9564208a7e8e6a7f637538
Gjorge <dimitrov.gjorge@protonmail.com>
2023-03-18 10:39:05 +0100

    reformated

--- G11/Class_08/recap/routes/motors-bikes.routes.js
+++ G11/Class_08/recap/routes/motors-bikes.routes.js
@@ -7,31 +7,36 @@ const motorsController = new MotorsController();
 
 //GET ALL => localhost:3000/motors
 motorBikesRouter.get("/", async (req, res) => {
-    const motors = await motorsController.getMotors()
+  const motors = await motorsController.getMotors();
 
-    res.status(200).send(motors)
-})
+  res.status(200).send(motors);
+});
 
 // GET BY ID => localhost:3000/motors/:id
-motorBikesRouter.get("/:id", async(req, res) => {
-    const { id } = req.params;
-    
-    try {
-        const motor = await motorsController.getById(id)
-        res.send(motor)
+motorBikesRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    } catch (error) {
-        res.status(404).send({message: error.message})
-    }
-})
+  try {
+    const motor = await motorsController.getById(id);
+    res.send(motor);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
 
 // POST => localhost:3000/motors
-motorBikesRouter.post('/', async (req, res) => {
-    const body = req.body;
+motorBikesRouter.post("/", async (req, res) => {
+  const body = req.body;
 
-    await motorsController.addMotor(body.model, body.make, body.year, body.engineSize, body.color, body.price);
+  await motorsController.addMotor(
+    body.model,
+    body.make,
+    body.year,
+    body.engineSize,
+    body.color,
+    body.price
+  );
 
-
-    res.status(201).send({message:"New motor was added"})
-})
+  res.status(201).send({ message: "New motor was added" });
+});
 export default motorBikesRouter;
