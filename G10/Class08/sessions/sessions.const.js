cb5782ccfbfae7895a05697501c74158bba3b47e
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-03-21 18:33:05 +0100

    class 8 break 1

---
+++ G10/Class08/sessions/sessions.const.js
@@ -0,0 +1,11 @@
+import session from 'express-session';
+
+export const productsSession = session({
+    secret: 'our_secret_123',
+    name: 'products_session',
+    cookie: {
+        maxAge: 5 * 60 * 60 * 1000
+    },
+    saveUninitialized: true,
+    resave: true,
+})
