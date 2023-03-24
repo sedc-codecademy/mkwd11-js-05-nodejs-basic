6be4196ddf5d541bc015538c1d530590b5a400de
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-03-24 19:36:57 +0100

    Students SSR app break 2

--- G10/Class11/index.js
+++ G10/Class11/index.js
@@ -8,6 +8,8 @@ const PORT = 3000;
 const HOSTNAME = 'localhost';
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs');
 
--- G10/Class11/routes/student.routes.js
+++ G10/Class11/routes/student.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import DataService from '../service/data.service.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router()
 
@@ -7,6 +8,41 @@ router.get('/', async (req, res) => {
     const students = await DataService.readFile('./data/students.json')
 
     res.status(200).render('student-list', { students })
+})
+
+router.get('/add-student', (req, res) => {
+    res.status(200).render('add-student-form')
+})
+
+router.post('/add-student', async (req, res) => {
+    const studentData = req.body;
+    
+    console.log(studentData)
+
+    const students = await DataService.readFile('./data/students.json');
+
+    const student = {
+        ...studentData,
+        id: uuidv4()
+    }
+
+    students.push(student)
+
+    await DataService.writeFile('./data/students.json', students);
+
+    res.redirect('/students')
+})
+
+router.get('/:id', async (req, res) => {
+    const students = await DataService.readFile('./data/students.json');
+
+    const student = students.find(s => s.id === req.params.id);
+
+    if (student) {
+        res.status(200).render('student-details', { student })
+    } else {
+        res.status(404).render('not-found')
+    }
 })
 
 export default router;
