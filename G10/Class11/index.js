e41ca6c90202005a0a95c002b16069962ab8ebf4
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-03-24 22:46:26 +0100

    Node js basic DONE :)

--- G10/Class11/index.js
+++ G10/Class11/index.js
@@ -7,6 +7,8 @@ const app = express()
 const PORT = 3000;
 const HOSTNAME = 'localhost';
 
+app.use(express.static('./public'))
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
